116ac76c1d4ac302e109ce6a6802471214c6bbed
Erick .oG Gutierrez <113930446+ErickG1234@users.noreply.github.com>
2022-10-20 17:03:38 +0000

    most recent 10/19/2022

--- main.py
+++ main.py
@@ -1,62 +1,47 @@
 # The task is this: the program will ask for the user's name and then it will say
-
-
-secretNumber = randint(1,101)
-name = input("What is your name?")
+from random import randint
+import math
+count = 0
+secretNumber = randint(1, 101)
+name = input("What is your name? ")
 print(name)
 
 print("Well, " + name + " I've thought of a number between 1 and 100")
 print("You have only eight tries to guess it.")
-guess = input("What number do you think it is?")
-print(guess)
-
+guess = input("What number do you think it is? ")
+integer_guess = int(guess)
+print("You guessed: " + str(integer_guess))
 
 # 1 If the number the user said is less than 1 or greater than 100, it will tell them that  he/she has chosen a number that is out of play.
-count = 0
-for guess in secretNumber(count):
-  if guess != secretNumber:
+if integer_guess < 1 or integer_guess > 100:
+  print("Out of play")
+
+for guess in secretNumber:
+  if integer_guess != secretNumber:
     count = count + 1
-  elif count == 8:
+  elif count > 8:
     print("You lose! Better luck next time.")
-    
-          
-    
-  
-  
+  else: 
+    print("You lose") 
 
-
-
-
-
-if guess >= secretNumber():
+if integer_guess >= secretNumber():
   print("Your guess is higher than the number I've thought of")
 else:
   print("Your guess is smaller than the number I've thought of")
-  
-  
-  
-
 
 # 2 If the number the user chose is less than the number the program thought of, it will tell them that the answer is wrong, and that he/she chose a lower number than the secret number.
 
-    
-if guess >= secretNumber:
+if integer_guess >= secretNumber:
   print("Your guess is higher than the number I've thought of")
 
-  
 # 3 If the user chose a number greater than the secret number, it will let them know that it was greater.
 
-  
-if guess >= secretNumber:
+if integer_guess >= secretNumber:
   print("Your guess is higher than the number I've thought of")
 
-  
 # 4 And if the user got the secret number right, they will be informed that they have won,and how many tries that has taken them.
 
-  
-if guess == secretNumber:
-  print("You got it right! Congradulations")
-
-
+if integer_guess == secretNumber:
+  print("You got it right! Congratulations")
 
 # 5 If the user has not guessed correctly in their first attempt, they will be asked again to choose another number and so on until they win or until their eight attempts are done.
